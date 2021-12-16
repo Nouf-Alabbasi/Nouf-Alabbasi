8547b26c08eb97edc676cb02f4321acfc8b44192
Nouf Alabbasi <naa475@nyu.edu>
2021-12-16 11:02:13 +0400

    fixed loading page

--- main.js
+++ main.js
@@ -5,10 +5,11 @@ var myVar;
 window.onload =  function() {Allfunc()};
 
 	function Allfunc() {
-		document.getElementById("loader").style.display  = "none";
-		document.getElementById("mainpage").style.display = "block";
-		document.getElementById("demo").style.opacity='1';
-		document.getElementById("projTitle").style.opacity='1';
+		// document.getElementById("loader").style.display  = "none";
+		// document.getElementById("mainpage").style.display = "block";
+		// document.getElementById("demo").style.opacity='1';
+		// document.getElementById("projTitle").style.opacity='1';
+	  myVar = setTimeout(showPage, 3000);
 	}
 
 	function showPage() {
